9670cb35febd0774c60a07562caa19f49b642ac6
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-07 15:35:20 -0400

    Create users table

--- Postgres/init/init-db.sql
+++ Postgres/init/init-db.sql
@@ -6,3 +6,17 @@ CREATE DATABASE gift_guardian;
 
 -- Connect to the gift_guardian database
 \c gift_guardian
+
+-- Setup DB
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    last_login TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create index on email for faster lookups
+CREATE INDEX idx_users_email ON users(email);
