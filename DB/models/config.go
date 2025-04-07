d9d483493a30e469b42371d580f3fc6ac87a4c7b
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-07 11:51:14 -0400

    IP isn't needed, only use port

--- DB/models/config.go
+++ DB/models/config.go
@@ -2,21 +2,19 @@ package models
 
 import (
 	"fmt"
-
-	ComModels "github.com/benjaminRoberts01375/Go-Communicate/models"
 )
 
 type Config struct {
-	Address       ComModels.Address `json:"db_address"`
-	DBName        string            `json:"db_name"`
-	User          string            `json:"user"`
-	Password      string            `json:"password"`
-	ContainerName string            `json:"container_name"`
+	DBPort        int    `json:"db_port"`
+	DBName        string `json:"db_name"`
+	User          string `json:"user"`
+	Password      string `json:"password"`
+	ContainerName string `json:"container_name"`
 }
 
 func (config Config) PSQLInfo() string {
 	return fmt.Sprintf(
 		"postgresql://%s:%s@%s:%d/%s?sslmode=disable",
-		config.User, config.Password, config.ContainerName, config.Address.Port, config.DBName,
+		config.User, config.Password, config.ContainerName, config.DBPort, config.DBName,
 	)
 }
