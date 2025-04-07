9d8b44802738e60db933198459b6fa02d031defe
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-07 14:34:39 -0400

    Initial DB connection

--- DB/go.mod
+++ DB/go.mod
@@ -3,3 +3,5 @@ module github.com/benjaminRoberts01375/Gift-Guardian/DB
 go 1.24.2
 
 require github.com/benjaminRoberts01375/Go-Communicate v0.0.0-20250406175748-379c2a8bdbf4
+
+require github.com/lib/pq v1.10.9
--- DB/main.go
+++ DB/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
+	_ "github.com/lib/pq"
+
+	"github.com/benjaminRoberts01375/Gift-Guardian/DB/models"
 	Coms "github.com/benjaminRoberts01375/Go-Communicate"
 )
 
+var database *sql.DB
+var config models.Config
+
 func main() {
 	Coms.ReadConfig()
+	Coms.ReadExternalConfig("db.json", &config)
+	database = setupDB()
+	defer database.Close()
 	Coms.Println("Going up")
 	http.ListenAndServe(Coms.GetLaunchPort(), nil)
+}
+
+func setupDB() *sql.DB {
+	db, err := sql.Open("postgres", config.PSQLInfo())
+	if err != nil {
+		panic("Could not connect to DB: " + err.Error())
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic("Could not ping DB at " + config.PSQLInfo() + ": " + err.Error())
+	}
+	return db
 }
