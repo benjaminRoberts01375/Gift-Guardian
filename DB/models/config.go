bfd685031088841d075eeebe1066805715c5ba54
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-08 10:17:32 -0400

    Update DB Config field names

--- DB/models/config.go
+++ DB/models/config.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Config struct {
-	DBPort        int    `json:"db_port"`
-	DBName        string `json:"db_name"`
-	User          string `json:"user"`
-	Password      string `json:"password"`
-	ContainerName string `json:"container_name"`
+	DBPort          int    `json:"db_port"`
+	DBName          string `json:"db_name"`
+	DBUser          string `json:"user"`
+	DBPassword      string `json:"password"`
+	DBContainerName string `json:"container_name"`
 }
 
 func (config Config) PSQLInfo() string {
 	return fmt.Sprintf(
 		"postgresql://%s:%s@%s:%d/%s?sslmode=disable",
-		config.User, config.Password, config.ContainerName, config.DBPort, config.DBName,
+		config.DBUser, config.DBPassword, config.DBContainerName, config.DBPort, config.DBName,
 	)
 }
