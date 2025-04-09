b0f95dba9dde3ecbdc5699f5b8030839e82f78c1
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-09 14:52:42 -0400

    Initial add list button

---
+++ Frontend/src/components/add-list-button.tsx
@@ -0,0 +1,22 @@
+import addListButtonStyles from "./add-list-button.module.css";
+import "../style.css";
+import { useList } from "../context-template.tsx";
+import List from "../types/list.tsx";
+import User from "../types/user.tsx";
+
+const AddListButton = () => {
+  const { addList } = useList();
+
+  const createNewList = () => {
+    console.log("Creating new list");
+    addList(new List(new User("ben", "Benjamin", "Roberts"))); // TODO: Replace with actual user
+  };
+
+  return (
+    <button onClick={createNewList} id={addListButtonStyles["add-list-button"]}>
+      +
+    </button>
+  );
+};
+
+export default AddListButton;
