d938ab32f2769fb599a55d5be571c732cb3c80ab
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-09 14:52:55 -0400

    Initial collapsed list component

---
+++ Frontend/src/components/list-collapsed.tsx
@@ -0,0 +1,19 @@
+import listStyles from "./list.module.css";
+import "../style.css";
+import { useList } from "../context-template.tsx";
+
+interface ListCollapsedProps {
+  listID: string;
+}
+
+const ListCollapsed = ({ listID }: ListCollapsedProps) => {
+  const { getList } = useList();
+  const list = getList(listID);
+  return (
+    <div id={listStyles["List-entry"]}>
+      <h1>{list?.title}</h1>
+    </div>
+  );
+};
+
+export default ListCollapsed;
