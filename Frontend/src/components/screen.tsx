3a225570de4327ffc58c5af92efb487f513243b7
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-09 12:06:59 -0400

    Add screen component

---
+++ Frontend/src/components/screen.tsx
@@ -0,0 +1,28 @@
+import screenStyles from "./screen.module.css";
+import "../style.css";
+import companyLogo from "../assets/Wide.png";
+
+type ScreenProps = {
+  content: React.ReactNode;
+  title: string;
+};
+
+const Screen = ({ content, title }: ScreenProps) => {
+  return (
+    <div>
+      <div id={screenStyles["header"]}>
+        <img
+          src={companyLogo}
+          alt="GG Logo"
+          draggable="false"
+          id={screenStyles["gg-logo"]}
+        />
+        <h1 id={screenStyles["title"]}>{title}</h1>
+        <p id={screenStyles["profile"]}>Placeholder</p>
+      </div>
+      {content}
+    </div>
+  );
+};
+
+export default Screen;
