d3af86a05a4b3109817ae9f16ae6c3c835e5f882
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-10 14:10:53 -0400

    Allow for storing lists, groups, and gifts

--- Postgres/init/init-db.sql
+++ Postgres/init/init-db.sql
@@ -18,3 +18,29 @@ CREATE TABLE users (
 
 -- Create index on email for faster lookups
 CREATE INDEX idx_users_email ON users(email);
+
+-- Create a table for storing lists which hold groups of gifts
+CREATE TABLE lists (
+    id SERIAL PRIMARY KEY,
+    owner_id INTEGER REFERENCES users(id),
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create a table for storing groups
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    list_id INTEGER REFERENCES lists(id),
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create a table for storing gifts
+CREATE TABLE gifts (
+    id SERIAL PRIMARY KEY,
+    group_id INTEGER REFERENCES groups(id),
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    gotten BOOLEAN DEFAULT FALSE
+);
