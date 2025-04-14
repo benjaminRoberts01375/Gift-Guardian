74f8e1c1e2034149c40a057fbb0a648fdf3f5ed0
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-14 15:04:20 -0400

    Rename useLists to useList

--- Frontend/src/context-hook.tsx
+++ Frontend/src/context-hook.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { ListsContext, ListsContextType } from "./context-object.tsx";
 
 // Custom hook to use the context
-export const useLists = (): ListsContextType => {
+export const useList = (): ListsContextType => {
   const context = useContext(ListsContext);
   if (context === undefined) {
     throw new Error("useLists must be used within a ListsProvider");
