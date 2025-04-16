ebeb0178972ad897a0279c44f349d0ea685bb0ee
benjaminroberts01375 <benroberts01375@gmail.com>
2025-04-16 14:26:32 -0400

    Add client ID

--- Frontend/src/types/gift.tsx
+++ Frontend/src/types/gift.tsx
@@ -2,11 +2,13 @@ class Gift {
 	id: string;
 	name: string;
 	url: string;
+	clientID: string;
 
 	constructor(name: string = "", id: string = "") {
 		this.id = id;
 		this.name = name;
 		this.url = "";
+		this.clientID = crypto.randomUUID();
 	}
 }
 
