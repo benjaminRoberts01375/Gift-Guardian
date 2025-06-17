572bbfbad8115b271d52ddda4258631e50da9838
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-06-17 10:57:56 -0400

    Set default values for name and url

--- Frontend/src/components/gift-view.tsx
+++ Frontend/src/components/gift-view.tsx
@@ -1,5 +1,6 @@
 import "../style.css";
 import GiftStyles from "./gift-view.module.css";
+import { useList } from "../context-hook.tsx";
 
 interface ListGiftProps {
 	listID: string;
@@ -12,11 +13,23 @@ const GiftView = ({ listID, groupID, giftID }: ListGiftProps) => {
 		event.preventDefault();
 		console.log("Submitting for", listID, groupID, giftID);
 	};
+	const { giftGet } = useList();
+	const gift = giftGet(listID, groupID, giftID);
 
 	return (
 		<form id={GiftStyles["inputs"]} className="layer-down">
-			<input type="text" placeholder="Item Name" className={GiftStyles["field"]} />
-			<input type="text" placeholder="URL or Location" className={GiftStyles["field"]} />
+			<input
+				defaultValue={gift?.name ?? "Untitled Gift"}
+				type="text"
+				placeholder="Item Name"
+				className={GiftStyles["field"]}
+			/>
+			<input
+				defaultValue={gift?.url ?? ""}
+				type="text"
+				placeholder="URL or Location"
+				className={GiftStyles["field"]}
+			/>
 			<input type="text" placeholder="Notes" className={GiftStyles["field"]} />
 			<button role="submit" onClick={handleSubmit} id={GiftStyles["save"]}>
 				Save
